4684657873468f402e32f1127d3c40c46a0cd46c
KahfiSmith <alkahfii2018@gmail.com>
2024-05-08 21:47:29 +0700

    feat: installation tailwind with CLI

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        background: "rgb(15, 23, 42)",
+        primary: "rgb(30, 41, 59)",
+        secondary: "rgb(43, 55, 73)",
+        icon: "rgb(45, 212, 194)",
+        color: "rgb(203, 213, 225)",
+        border: "rgb(76, 90, 110)",
+        tag: "rgb(25, 51, 64)",
+        soft: "rgb(22, 32, 50)",
+        light: "rgb(248, 250, 252)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lora: ["Lora", "sans-serif"],
+      },
+      borderWidth: {
+        1: "1.5px",
+      },
+    },
+  },
+  plugins: [],
+};
