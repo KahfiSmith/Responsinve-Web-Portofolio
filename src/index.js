5527149a8d9000eb7cfec11e6880fce0f56e3ce7
KahfiSmith <alkahfii2018@gmail.com>
2024-05-12 14:52:51 +0700

    feat menyelesaikan navigasi hamburger ketika dilayar mobile

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("hamburger");
   const iconMenu = document.getElementById("icon-menu");
   const iconClose = document.getElementById("icon-close");
+  const navMenu = document.getElementById("nav-menu");
+
+  button.addEventListener("click", function () {
+    navMenu.classList.toggle("hidden");
+  });
 
   button.addEventListener("click", function () {
     iconMenu.classList.toggle("hidden");
