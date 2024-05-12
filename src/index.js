fcc865157d1929107156228cd3df6f7c9f23c52a
KahfiSmith <alkahfii2018@gmail.com>
2024-05-12 14:14:54 +0700

    feat: menambahkan navbar tranparant

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("hamburger");
-    const iconMenu = document.getElementById("icon-menu");
-    const iconClose = document.getElementById("icon-close");
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("hamburger");
+  const iconMenu = document.getElementById("icon-menu");
+  const iconClose = document.getElementById("icon-close");
 
-    button.addEventListener("click", function() {
-        iconMenu.classList.toggle("hidden");
-        iconClose.classList.toggle("hidden");
-    });
+  button.addEventListener("click", function () {
+    iconMenu.classList.toggle("hidden");
+    iconClose.classList.toggle("hidden");
+  });
 
-    window.onscroll = function() {
-        const header = document.querySelector("header");
-        const fixed = header.offsetTop;
+  window.onscroll = function () {
+    const header = document.querySelector("header");
+    const fixedNav = header.offsetTop;
 
-        if (window.pageYoff > fixedNav) {
-            header.classList.add("navbar-fixed");
-        } else {
-            header.classList.remove("navbar-fixed");
-        }
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add("navbar-fixed");
+    } else {
+      header.classList.remove("navbar-fixed");
     }
+  };
 });
